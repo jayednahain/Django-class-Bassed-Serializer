98f2c6363cbb87a71825fff10d7518a5c59f91cc
RAB_X69 <jayednahian@yahoo.com>
2021-03-21 18:29:12 +0600

    Update Django_class_Bassed_Serializer.sql

--- Django_class_Bassed_Serializer.sql
+++ Django_class_Bassed_Serializer.sql
@@ -1,3 +1,13 @@
 create database studentdb_new
 use studentdb_new
+#table name is created autometically
+#table name will app name + model name
 show tables
+#use the specifice table name for insert the data
+select * from class_based_views_student
+insert into class_based_views_student values(1,'jayed',80)
+insert into class_based_views_student values(2,'nahian',90)
+insert into class_based_views_student values(3,'chowdhury',60)
+insert into class_based_views_student values(4,'hasib',50)
+
+select * from class_based_views_student
